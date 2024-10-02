85978c06081e711a8b0709b8fedf0e6a025ff2db
bernoussama <oussama.bernou@outlook.com>
2024-10-02 21:50:43 +0100

    task 6-final-user

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signupUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signupUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((r) => ({
+        status: r.status,
+        value: r.status === 'fulfilled' ? r.value : r.reason,
+      }
+      ))
+    ));
+}
