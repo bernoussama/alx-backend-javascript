1806fc65158e04bdab831c513b21cbc2b91b34f7
bernoussama <oussama.bernou@outlook.com>
2024-10-03 21:37:42 +0100

    task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name; // eslint-disable-line no-underscore-dangle
+    this._length = length; // eslint-disable-line no-underscore-dangle
+    this._students = students; // eslint-disable-line no-underscore-dangle
+  }
+
+  get name() {
+    return this._name; // eslint-disable-line no-underscore-dangle
+  }
+
+  get length() {
+    return this._length; // eslint-disable-line no-underscore-dangle
+  }
+
+  get students() {
+    return this._students; // eslint-disable-line no-underscore-dangle
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName; // eslint-disable-line no-underscore-dangle
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength; // eslint-disable-line no-underscore-dangle
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || newStudents.some((student) => typeof student !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents; // eslint-disable-line no-underscore-dangle
+  }
+}
