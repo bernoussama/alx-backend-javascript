da47e79d4e3a456c247ede9693f97f3bec4c02d4
bernoussama <oussama.bernou@outlook.com>
2024-10-03 22:42:11 +0100

    task 7-airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name; // eslint-disable-line no-underscore-dangle
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = name; // eslint-disable-line no-underscore-dangle
+  }
+
+  get code() {
+    return this._code; // eslint-disable-line no-underscore-dangle
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = code; // eslint-disable-line no-underscore-dangle
+  }
+
+  toString() {
+    return this.code;
+  }
+}
