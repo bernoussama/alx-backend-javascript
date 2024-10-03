124de7eb648623098a7ef61dd07aedf34ece092a
bernoussama <oussama.bernou@outlook.com>
2024-10-03 22:44:54 +0100

    fix task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -26,7 +26,7 @@ export default class Airport {
     this._code = code; // eslint-disable-line no-underscore-dangle
   }
 
-  toString() {
+  get [Symbol.toStringTag]() {
     return this.code;
   }
 }
