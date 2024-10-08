04f78c3cadc8a0e949cb6516af662ab25941092f
bernoussama <oussama.bernou@outlook.com>
2024-10-08 23:37:40 +0100

    task 9

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,6 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) return '';
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
   return [...set].filter((el) => el.startsWith(startString)).map((el) => el.slice(startString.length)).join('-');
 }
