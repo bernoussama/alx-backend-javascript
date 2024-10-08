cbc11f9304c663d2c506c7e6693aa5a5dc239fbc
bernoussama <oussama.bernou@outlook.com>
2024-10-09 00:02:11 +0100

    update

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,5 @@
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) throw new Error('map must be a Map');
+  if (!(map instanceof Map)) throw new Error('Cannot process');
 
   map.forEach((value, key) => (value === 1 ? map.set(key, 100) : {}));
-  return map;
 }
