7640f0d4865eb99a8b66ab7534edca0b62b9ed9b
bernoussama <oussama.bernou@outlook.com>
2024-10-09 00:22:33 +0100

    update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,8 @@ export const weakMap = new WeakMap();
 const MAX = 5;
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
-  if (weakMap.get(endpoint) >= MAX) throw new Error('Endpoint load is high');
 
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+
+  if (weakMap.get(endpoint) >= MAX) throw new Error('Endpoint load is high');
 }
