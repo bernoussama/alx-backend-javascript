817003adf828c196f3e7585749d83e07f5f21323
bernoussama <oussama.bernou@outlook.com>
2024-10-09 00:26:06 +0100

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,5 +2,5 @@ export default function cleanSet(set, startString) {
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
-  return [...set].filter((el) => el.startsWith(startString)).map((el) => el.slice(startString.length)).join('-');
+  return [...set].filter((el) => el.startsWith(startString)).map((el) => (el.slice(startString.length) !== el ? el.slice(startString.length) : {})).join('-');
 }
