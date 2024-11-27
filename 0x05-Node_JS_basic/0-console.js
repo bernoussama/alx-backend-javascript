68c916b5850e77e997529943d634da6ce4ce8984
bernoussama <oussama.bernou@outlook.com>
2024-11-27 01:35:49 +0100

    update

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * prints in stdout the string argument
+ * @param {string} message
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,4 @@
+# alx-backend-javascript
+this repo contains backend projects with javascript
+## License
+MIT
